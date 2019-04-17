2820fecb23dc9977e97c5cf32022666c1172c030
showboat051 <rashad.davis87@yahoo.com>
2019-04-17 01:48:04 -0500

    2nd commit

---
+++ 2019weather/src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+class Header extends React.Component {
+
+    render() {
+
+        return(
+
+            <div>
+                <h1>Texas Weather... What is it?</h1>
+
+            </div>
+
+        )
+
+    }
+    
+}
+
+export default Header;

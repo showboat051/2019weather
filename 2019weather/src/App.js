cc98bc9aa70aa281f26cc2014a23ca84843abdce
showboat051 <rashad.davis87@yahoo.com>
2019-04-18 00:54:01 -0500

    added api in a form

--- 2019weather/src/App.js
+++ 2019weather/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Form from './components/Form/Form';
 
 class App extends Component {
   render() {
     return (
         <div>
           <Header />
-
+          <Form />
         </div> 
     )
   }
---
+++ 2019weather/src/components/Form/Form.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class Form extends React.Component {
+
+    state = {
+        temperature: ""
+    };
+
+
+    async componentDidMount() {
+
+        const url = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=ff05166f3c7814d58199e22969528af4'
+         const response = await fetch(url);
+         const data = await response.json();
+         console.log(data);
+    }
+    
+    render() {
+
+        return(
+            <form>
+                <input type="text" name="city" placeholder= "City"/>
+                <button>Weather</button>
+
+            </form>
+        );
+
+    }
+
+};
+
+
+export default Form;
